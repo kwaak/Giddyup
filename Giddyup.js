05981113f61e22adc3777a59cb214af904143bb0
kwaak <taco.perquin@gmail.com>
2010-05-05 16:52:52 +0200

    Removed homegrown websocket server, and implemented Socket.IO-node

--- Giddyup.js
+++ Giddyup.js
@@ -3,7 +3,8 @@ var sys = require('sys');
 var http = require('http');
 var tcp = require('tcp');
 var fs = require("fs");
-
+var io = require('../Socket.IO-node.git/socket.io.js'
+    
 // Giddyup
 var visitorList = require('./VisitorList');
 var tools = require('./Tools');
@@ -78,117 +79,9 @@ var responseHeaders = [
   ''
 ];
 
-// TCP (Websockets) Server
-var tcpServer = tcp.createServer(function(socket) {
-  socket.setEncoding("utf8");
-  socket.setNoDelay(true);
-  
-  this.handshaked = false;
-  this.data = "";
-  
-  socket.addListener("connect", function () {
-    sys.debug("New TCP connection");
-  });
-  socket.addListener("data", function (data) {
-
-    // Perform a proper handshake
-    if(!this.handshaked)
-    {
-      sys.debug("Performing handshake");
-      
-      var headers = data.split('\r\n');
-      var matches = [];
-
-      // Check for suitable headers
-      for(var i = 0, l = headers.length, match; i < l; i++)
-      {
-        if (i === requestHeaders.length) break; // handle empty lines that UA send 
-        match = headers[i].match(requestHeaders[i]);
-        if (match && match.length > 1)
-        {
-          // if there's a capture group, push it into the matches          
-          matches.push(match[1]);
-        } 
-        else if (!match) 
-        {
-          sys.debug('Handshake aborted. Bad header ' + headers[i]);          
-          this.socket.close()
-          return false;
-        }
-      }
-      
-      // Write a proper succesfull handshake response
-      this.write(tools.substitute(responseHeaders.join('\r\n'), {
-        resource: matches[0],
-        host: matches[1],
-        origin: matches[2],
-        protocol: 'ws'
-      }));
-      
-      this.handshaked = true;
-      sys.debug('Handshake sent', 'info');
-      
-      var self = this;
-      var timeFunc = function() { self.write('\u0000Eeyoo!\uffff'); setTimeout(timeFunc, 1000); }
-      setTimeout(timeFunc, 1000);
-      
-      return true;
-    }
-    else
-    {
-      sys.debug("Receiving data: ");
-      
-      // Handle the data
-      this.data += data;
-      var self = this;
-      
-      // Split the data out of the chunks
-      chunks = data.split('\ufffd');
-      sys.debug(sys.inspect(chunks));
-      chunks.pop();
-      chunks.forEach(function (chunk) {
-        sys.debug(chunk[1]);
-        if (chunk[0] != '\u0000') {
-          sys.debug("Invalid chunk received");
-          self.close();
-          return false;
-        }
-
-        var chunkData = chunk.substr(1, chunk.length);
-        //self.write('\u0000' + chunk.substr(1, chunk.length) + '\uffff');
-      });
-        
-      return true;
-    }
-  });
-  socket.addListener("end", function () {
-    sys.debug("TCP connection closed");
-    this.close();
-  });
-});
-
-
-function stringToBytes ( str ) {
-  var ch, st, re = [];
-  for (var i = 0; i < str.length; i++ ) {
-    ch = str.charCodeAt(i);  // get char 
-    st = [];                 // set up "stack"
-    do {
-      st.push( ch & 0xFF );  // push byte to stack
-      ch = ch >> 8;          // shift value down by 1 byte
-    }  
-    while ( ch );
-    // add stack contents to result
-    // done because chars have "wrong" endianness
-    re = re.concat( st.reverse() );
-  }
-  // return an array of bytes
-  return re;
-}
-
 // Start the servers
 httpServer.listen(80);
 sys.log('HTTP Server running at port 80');
 
-tcpServer.listen(8080);
-sys.log("TCP Server running at port 8080");
+// Start the socket server (websockets)
+io.listen(httpServer);
